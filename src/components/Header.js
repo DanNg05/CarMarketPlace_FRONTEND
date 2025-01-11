264ed6bd3a62f93ddc03b52bd1219bf7a7d9a341
Dan Nguyen <sydan05071997@gmail.com>
2025-01-11 20:43:26 +1030

    header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import carMarket from '../photos/CarMarket.png'
+
+const Header = () => {
+  return (
+    <div className="container">
+      <img src={carMarket} alt="Car Market Logo" style={{ width: '100px', maxWidth: '500px', height: 'auto' }} />
+
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Homepage = () => {
   return (
     <div>
+      <Header/>
       <h1>Welcome to the Homepage!</h1>
     </div>
   );
